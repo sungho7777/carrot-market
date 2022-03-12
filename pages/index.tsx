4245313d42e8ffd8d941362070632bf61b5d4752
Sung Ho Park <sunghopark@Sungui-MacBookPro.local>
2022-03-12 18:21:42 +0900

    ## 4.9 Responsive Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,95 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-2 p-5">
+    <div className="bg-slate-300 py-20 px-10 grid gap-10 min-h-screen">
       
-      <details className="select-none open:text-white open:bg-indigo-400">
-        <summary className="select-none cursor-pointer">What is my fav. food.</summary>
-        <span>김치</span>
-      </details>
+      <div className="bg-white sm:bg-red-400 md:bg-teal-400 lg:bg-green-400 p-6 xl:bg-indigo-400 2xl:bg-purple-400 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Seelct Item</span>
+        <ul>
+          {[1,2,3,4,5,6,7].map((i)=>(
+            <div className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-50">
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
+        <ul>
+          {["a", "b", "c", ""].map((c, i)=>(
+            <li className="bg-red-50 py-2 empty:hidden" key={i}>
+              {c}
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$38</span>
+        </div>
+        <button className="
+            mt-5 bg-blue-500 text-white 
+            p-3 text-center rounded-xl 
+            w-2/4 mx-auto 
+            hover:bg-teal-500 hover:text-black 
+            active:bg-yellow-500 focus:bg-red-500
+          ">Checkout</button>
+      </div>
 
-      <ul className="list-decimal marker:text-teal-500">
-        <li>hi</li>
-        <li>hi</li>
-        <li>hi</li>
-        <li>hi</li>
-      </ul>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-14">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex float-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">$340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-red-300 transition-colors" />
+            <div className="flex float-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$2,310</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+              <span className="text-lg font-medium">Tony Molloy</span>
+              <span className="text-sm text-gray-500">New York, USA</span>
+            </div>
+        </div>
+      </div>
 
-      <input 
-        type="file" 
-        className="
-          file:cursor-pointer 
-          file:hover:text-purple-400 
-          file:hover:bg-white
-          file:hover:border    
-          file:transition-colors 
-          file:border-0 
-          file:rounded-xl 
-          file:p-2 
-          file:text-white 
-          file:bg-purple-400" 
-        />
-
-      <div className="first-letter:text-7xl first-letter:hover:text-purple-500">Lorem Ipsum is simply dummy text.</div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>★ 4.9</span>
+            <span className="shadow-xl p-2 rounded-md">❤️</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-80 mb-5" />
+        <div className="flex flex-col">
+          <span className="font-medium text-lg">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500 bg-opacity-50 focus:ring-2 ring-offset-1 ring-yellow-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500 bg-opacity-50 focus:ring-2 ring-offset-1 ring-indigo-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500 bg-opacity-50 focus:ring-2 ring-offset-1 ring-teal-500 transition"></button>
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                -
+              </button>
+              <span>11</span>
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-lg">Add to cart</button>
+          </div>
+        </div>
+      </div>
 
     </div>
   );
